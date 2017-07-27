52929f976d782b1838fb45285727b922345df61f
Augusto Azevedo & Ginny Fahs <pair+aazevedojr.ginnyfahs@devbootcamp.com>
2017-07-27 16:32:25 -0700

    Created login, logout, custom errors

---
+++ app/helpers/sessions_helpers.rb
@@ -0,0 +1,15 @@
+helpers do
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login(user)
+    session[:user_id] = user.id
+  end
+
+end
